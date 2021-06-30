3122e23d4ef0b13080883eda6cae682f8055c989
Jitesh Prajapat <60288640+jiteshp13@users.noreply.github.com>
2021-06-30 13:35:56 +0530

    Add /getComments route

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -1,12 +1,25 @@
 const axios = require("axios");
 
-exports.getPosts = (req, res) => {
-  axios.get("https://jsonplaceholder.typicode.com/posts")
+exports.getPosts = async (req, res) => {
+  await axios.get("https://jsonplaceholder.typicode.com/posts")
     .then((data) => {
-      console.log(data);
+      // console.log(data);
       return res.status(200).send(data.data);
     })
     .catch((error) => {
       console.error("Error reteriving posts", error);
+      return res.status(500).send("Error reteriving posts");
+    });
+};
+
+exports.getComments = async (req, res) => {
+  await axios.get("http://localhost:7000/getPosts")
+    .then((data) => {
+      console.log(data);
+      return res.status(200).send("Comments sent");
+    })
+    .catch((error) => {
+      console.error("Error reteriving comments", error);
+      return res.status(500).send("Error reteriving posts");
     });
 };
--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -4,5 +4,6 @@ const postController = require("../controller/postController");
 router = express();
 
 router.get("/getPosts", postController.getPosts);
+router.get("/getComments", postController.getComments);
 
 module.exports = router;
