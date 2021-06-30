ede3301a3cb90477972e5e424878505db68257ed
Jitesh Prajapat <60288640+jiteshp13@users.noreply.github.com>
2021-06-30 13:01:17 +0530

    Add getPosts

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -1,4 +1,9 @@
 const axios = require("axios");
 
 exports.getPosts = (req, res) => {
+  axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then((data) => {
+      console.log(data);
+      return res.status(200).send("Posts collected");
+    })
 }
