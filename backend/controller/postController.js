09508c87fdfab32e0393945eb5f4fc9094892e91
Jitesh Prajapat <60288640+jiteshp13@users.noreply.github.com>
2021-06-30 13:08:28 +0530

    Return data.data

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -4,6 +4,9 @@ exports.getPosts = (req, res) => {
   axios.get("https://jsonplaceholder.typicode.com/posts")
     .then((data) => {
       console.log(data);
-      return res.status(200).send("Posts collected");
+      return res.status(200).send(data.data);
     })
-}
+    .catch((error) => {
+      console.error("Error reteriving posts", error);
+    });
+};
